b660035b967622e7076ead86d685a1efa79b101c
vilyLei <vily313@126.com>
2022-07-13 00:17:35 +0800

    fix: add some test samples code.

---
+++ ginDev/sample01/t07.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	// 限制上传最大尺寸
+	r.MaxMultipartMemory = 8 << 20
+	r.POST("/upload", func(c *gin.Context) {
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.String(500, "上传图片出错")
+		}
+		// c.JSON(200, gin.H{"message": file.Header.Context})
+		c.SaveUploadedFile(file, file.Filename)
+		c.String(http.StatusOK, file.Filename)
+	})
+	r.Run(":9090")
+}
