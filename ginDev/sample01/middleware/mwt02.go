16f1dacba23ed7d63a1d26b6a712bfa4e21586f4
vilyLei <vily313@126.com>
2022-07-15 21:50:56 +0800

    fix: add CORS policy support.

--- ginDev/sample01/middleware/mwt02.go
+++ ginDev/sample01/middleware/mwt02.go
@@ -33,11 +33,29 @@ func main() {
 }
 
 func shopIndexHandler(c *gin.Context) {
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+	if c.Request.Method == "OPTIONS" {
+		c.AbortWithStatus(204)
+		return
+	}
 	time.Sleep(5 * time.Second)
 	c.JSON(http.StatusOK, gin.H{"status": "200", "type": "shopIndexHandler"})
 }
 
 func shopHomeHandler(c *gin.Context) {
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+	if c.Request.Method == "OPTIONS" {
+		c.AbortWithStatus(204)
+		return
+	}
 	time.Sleep(3 * time.Second)
 	c.JSON(http.StatusOK, gin.H{"status": "200", "type": "shopHomeHandler"})
 }
